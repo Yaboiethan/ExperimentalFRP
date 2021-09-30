340531a6341e8d69f84bd53375834b9248bdee73
Yaboiethan <ethanMH2002@gmail.com>
2021-09-30 13:35:41 -0400

    Beginning Animation and Fixed Pivots
    Fixed T-Rex pivots and started the animation system

--- ForgeResourcePackage/src/main/java/com/idtech/entity/TRexModel.java
+++ ForgeResourcePackage/src/main/java/com/idtech/entity/TRexModel.java
@@ -1,15 +1,16 @@
-package com.idtech.entity;// Made with Blockbench 3.9.3
+package com.idtech.entity;
+// Made with Blockbench 3.9.3
 // Exported for Minecraft version 1.15 - 1.16 with MCP mappings
 // Paste this class into your mod and generate all required imports
 
 
+import com.idtech.entity.TRexEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-
-import java.util.function.Consumer;
+import net.minecraft.util.math.MathHelper;
 
 public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 	private final ModelRenderer Body;
@@ -61,27 +62,27 @@ public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 		BodySpike_r1.setTextureOffset(35, 36).addBox(-2.0F, -3.0F, -8.0F, 3.0F, 4.0F, 2.0F, 0.0F, false);
 
 		Head = new ModelRenderer(this);
-		Head.setRotationPoint(0.0F, 11.0F, 2.0F);
+		Head.setRotationPoint(0.0F, -4.0F, -6.0F);
 		Body.addChild(Head);
-		Head.setTextureOffset(0, 25).addBox(-3.0F, -17.4F, -15.2F, 5.0F, 2.0F, 7.0F, 0.0F, false);
-		Head.setTextureOffset(21, 22).addBox(-2.0F, -18.4F, -17.2F, 3.0F, 1.0F, 9.0F, 0.0F, false);
-		Head.setTextureOffset(41, 38).addBox(0.0F, -19.4F, -13.2F, 1.0F, 1.0F, 4.0F, 0.0F, false);
-		Head.setTextureOffset(24, 41).addBox(-2.0F, -19.4F, -13.2F, 1.0F, 1.0F, 4.0F, 0.0F, false);
-		Head.setTextureOffset(0, 16).addBox(-0.4F, -20.4F, -15.2F, 0.0F, 2.0F, 7.0F, 0.0F, true);
-		Head.setTextureOffset(35, 18).addBox(-3.0F, -16.4F, -17.2F, 5.0F, 1.0F, 2.0F, 0.0F, false);
-		Head.setTextureOffset(25, 0).addBox(-2.0F, -17.4F, -18.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(-3.0F, -15.4F, -17.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(-1.0F, -15.4F, -17.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(1.0F, -15.4F, -17.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(1.0F, -15.4F, -15.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(1.0F, -15.4F, -13.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(1.0F, -15.4F, -11.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(-3.0F, -15.4F, -11.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(-3.0F, -15.4F, -13.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		Head.setTextureOffset(14, 23).addBox(-3.0F, -15.4F, -15.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(0, 25).addBox(-3.0F, -2.4F, -7.2F, 5.0F, 2.0F, 7.0F, 0.0F, false);
+		Head.setTextureOffset(21, 22).addBox(-2.0F, -3.4F, -9.2F, 3.0F, 1.0F, 9.0F, 0.0F, false);
+		Head.setTextureOffset(41, 38).addBox(0.0F, -4.4F, -5.2F, 1.0F, 1.0F, 4.0F, 0.0F, false);
+		Head.setTextureOffset(24, 41).addBox(-2.0F, -4.4F, -5.2F, 1.0F, 1.0F, 4.0F, 0.0F, false);
+		Head.setTextureOffset(0, 16).addBox(-0.4F, -5.4F, -7.2F, 0.0F, 2.0F, 7.0F, 0.0F, true);
+		Head.setTextureOffset(35, 18).addBox(-3.0F, -1.4F, -9.2F, 5.0F, 1.0F, 2.0F, 0.0F, false);
+		Head.setTextureOffset(25, 0).addBox(-2.0F, -2.4F, -10.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(-3.0F, -0.4F, -9.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(-1.0F, -0.4F, -9.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(1.0F, -0.4F, -9.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(1.0F, -0.4F, -7.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(1.0F, -0.4F, -5.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(1.0F, -0.4F, -3.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(-3.0F, -0.4F, -3.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(-3.0F, -0.4F, -5.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		Head.setTextureOffset(14, 23).addBox(-3.0F, -0.4F, -7.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 
 		BottomJaw = new ModelRenderer(this);
-		BottomJaw.setRotationPoint(0.0F, -15.0F, -9.0F);
+		BottomJaw.setRotationPoint(0.0F, 0.0F, -1.0F);
 		Head.addChild(BottomJaw);
 		BottomJaw.setTextureOffset(20, 12).addBox(-2.0F, 1.6F, -9.2F, 3.0F, 1.0F, 9.0F, 0.0F, false);
 		BottomJaw.setTextureOffset(14, 23).addBox(1.0F, -0.4F, -5.2F, 1.0F, 1.0F, 1.0F, 0.0F, false);
@@ -97,19 +98,19 @@ public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 		BottomJaw.setTextureOffset(0, 12).addBox(-3.0F, 0.6F, -9.2F, 5.0F, 1.0F, 10.0F, 0.0F, false);
 
 		LeftUpperArm = new ModelRenderer(this);
-		LeftUpperArm.setRotationPoint(-6.0F, 0.0F, -4.0F);
+		LeftUpperArm.setRotationPoint(-6.0F, -2.0F, -3.0F);
 		Body.addChild(LeftUpperArm);
 		
 
 		LeftUpperArm_r1 = new ModelRenderer(this);
-		LeftUpperArm_r1.setRotationPoint(0.0F, -2.0F, 2.0F);
+		LeftUpperArm_r1.setRotationPoint(0.0F, 0.0F, 1.0F);
 		LeftUpperArm.addChild(LeftUpperArm_r1);
 		setRotationAngle(LeftUpperArm_r1, -0.5672F, 0.0F, 0.0F);
 		LeftUpperArm_r1.setTextureOffset(20, 12).addBox(-2.0F, 0.0F, -2.0F, 2.0F, 4.0F, 2.0F, 0.0F, false);
 		LeftUpperArm_r1.setTextureOffset(0, 0).addBox(0.0F, -1.0F, -2.0F, 1.0F, 5.0F, 2.0F, 0.0F, false);
 
 		LeftHand = new ModelRenderer(this);
-		LeftHand.setRotationPoint(0.0F, 0.0F, 0.0F);
+		LeftHand.setRotationPoint(0.0F, 2.0F, -1.0F);
 		LeftUpperArm.addChild(LeftHand);
 		
 
@@ -127,19 +128,19 @@ public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 		HandNail_r1.setTextureOffset(34, 42).addBox(-1.0F, 0.3386F, -0.6878F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
 		RightUpperArm = new ModelRenderer(this);
-		RightUpperArm.setRotationPoint(5.0F, 0.0F, -4.0F);
+		RightUpperArm.setRotationPoint(5.0F, -2.0F, -3.0F);
 		Body.addChild(RightUpperArm);
 		
 
 		RightUpperArm_r1 = new ModelRenderer(this);
-		RightUpperArm_r1.setRotationPoint(0.0F, -2.0F, 2.0F);
+		RightUpperArm_r1.setRotationPoint(0.0F, 0.0F, 1.0F);
 		RightUpperArm.addChild(RightUpperArm_r1);
 		setRotationAngle(RightUpperArm_r1, -0.5672F, 0.0F, 0.0F);
 		RightUpperArm_r1.setTextureOffset(20, 12).addBox(0.0F, 0.0F, -2.0F, 2.0F, 4.0F, 2.0F, 0.0F, true);
 		RightUpperArm_r1.setTextureOffset(0, 0).addBox(-1.0F, -1.0F, -2.0F, 1.0F, 5.0F, 2.0F, 0.0F, true);
 
 		RightHand = new ModelRenderer(this);
-		RightHand.setRotationPoint(0.0F, 0.0F, 0.0F);
+		RightHand.setRotationPoint(0.0F, 2.0F, -1.0F);
 		RightUpperArm.addChild(RightHand);
 		
 
@@ -157,46 +158,46 @@ public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 		HandNail_r2.setTextureOffset(34, 42).addBox(-1.0F, 0.3386F, -0.6878F, 2.0F, 3.0F, 2.0F, 0.0F, true);
 
 		LeftLeg = new ModelRenderer(this);
-		LeftLeg.setRotationPoint(-6.0F, 5.0F, 2.0F);
+		LeftLeg.setRotationPoint(-6.0F, 1.0F, 3.0F);
 		Body.addChild(LeftLeg);
-		LeftLeg.setTextureOffset(14, 41).addBox(0.0F, 1.0F, 0.0F, 3.0F, 4.0F, 2.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(0, 34).addBox(-1.0F, -4.0F, -1.0F, 4.0F, 5.0F, 4.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(36, 22).addBox(0.0F, 5.0F, -1.0F, 3.0F, 1.0F, 4.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(30, 32).addBox(-1.0F, 5.0F, -4.0F, 5.0F, 1.0F, 3.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(36, 27).addBox(0.0F, 4.0F, -3.0F, 3.0F, 1.0F, 3.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(17, 28).addBox(-1.0F, 4.0F, -4.0F, 5.0F, 1.0F, 1.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(0, 28).addBox(-1.0F, 5.0F, -6.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(25, 4).addBox(1.0F, 5.0F, -6.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(16, 34).addBox(1.0F, 4.0F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(12, 34).addBox(3.0F, 4.0F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(32, 10).addBox(-1.0F, 4.0F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(0, 25).addBox(3.0F, 5.0F, -6.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
-		LeftLeg.setTextureOffset(20, 34).addBox(1.0F, 5.0F, 3.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(14, 41).addBox(0.0F, 5.0F, -1.0F, 3.0F, 4.0F, 2.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(0, 34).addBox(-1.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(36, 22).addBox(0.0F, 9.0F, -2.0F, 3.0F, 1.0F, 4.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(30, 32).addBox(-1.0F, 9.0F, -5.0F, 5.0F, 1.0F, 3.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(36, 27).addBox(0.0F, 8.0F, -4.0F, 3.0F, 1.0F, 3.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(17, 28).addBox(-1.0F, 8.0F, -5.0F, 5.0F, 1.0F, 1.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(0, 28).addBox(-1.0F, 9.0F, -7.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(25, 4).addBox(1.0F, 9.0F, -7.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(16, 34).addBox(1.0F, 8.0F, -6.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(12, 34).addBox(3.0F, 8.0F, -6.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(32, 10).addBox(-1.0F, 8.0F, -6.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(0, 25).addBox(3.0F, 9.0F, -7.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+		LeftLeg.setTextureOffset(20, 34).addBox(1.0F, 9.0F, 2.0F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 
 		RightLeg = new ModelRenderer(this);
-		RightLeg.setRotationPoint(6.0F, 5.0F, 2.0F);
+		RightLeg.setRotationPoint(6.0F, 1.0F, 3.0F);
 		Body.addChild(RightLeg);
-		RightLeg.setTextureOffset(14, 41).addBox(-3.0F, 1.0F, 0.0F, 3.0F, 4.0F, 2.0F, 0.0F, true);
-		RightLeg.setTextureOffset(0, 34).addBox(-3.0F, -4.0F, -1.0F, 4.0F, 5.0F, 4.0F, 0.0F, true);
-		RightLeg.setTextureOffset(36, 22).addBox(-3.0F, 5.0F, -1.0F, 3.0F, 1.0F, 4.0F, 0.0F, true);
-		RightLeg.setTextureOffset(30, 32).addBox(-4.0F, 5.0F, -4.0F, 5.0F, 1.0F, 3.0F, 0.0F, true);
-		RightLeg.setTextureOffset(36, 27).addBox(-3.0F, 4.0F, -3.0F, 3.0F, 1.0F, 3.0F, 0.0F, true);
-		RightLeg.setTextureOffset(17, 28).addBox(-4.0F, 4.0F, -4.0F, 5.0F, 1.0F, 1.0F, 0.0F, true);
-		RightLeg.setTextureOffset(0, 28).addBox(0.0F, 5.0F, -6.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
-		RightLeg.setTextureOffset(25, 4).addBox(-2.0F, 5.0F, -6.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
-		RightLeg.setTextureOffset(16, 34).addBox(-2.0F, 4.0F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
-		RightLeg.setTextureOffset(12, 34).addBox(-4.0F, 4.0F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
-		RightLeg.setTextureOffset(32, 10).addBox(0.0F, 4.0F, -5.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
-		RightLeg.setTextureOffset(0, 25).addBox(-4.0F, 5.0F, -6.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
-		RightLeg.setTextureOffset(20, 34).addBox(-2.0F, 5.0F, 3.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
+		RightLeg.setTextureOffset(14, 41).addBox(-3.0F, 5.0F, -1.0F, 3.0F, 4.0F, 2.0F, 0.0F, true);
+		RightLeg.setTextureOffset(0, 34).addBox(-3.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F, 0.0F, true);
+		RightLeg.setTextureOffset(36, 22).addBox(-3.0F, 9.0F, -2.0F, 3.0F, 1.0F, 4.0F, 0.0F, true);
+		RightLeg.setTextureOffset(30, 32).addBox(-4.0F, 9.0F, -5.0F, 5.0F, 1.0F, 3.0F, 0.0F, true);
+		RightLeg.setTextureOffset(36, 27).addBox(-3.0F, 8.0F, -4.0F, 3.0F, 1.0F, 3.0F, 0.0F, true);
+		RightLeg.setTextureOffset(17, 28).addBox(-4.0F, 8.0F, -5.0F, 5.0F, 1.0F, 1.0F, 0.0F, true);
+		RightLeg.setTextureOffset(0, 28).addBox(0.0F, 9.0F, -7.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
+		RightLeg.setTextureOffset(25, 4).addBox(-2.0F, 9.0F, -7.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
+		RightLeg.setTextureOffset(16, 34).addBox(-2.0F, 8.0F, -6.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
+		RightLeg.setTextureOffset(12, 34).addBox(-4.0F, 8.0F, -6.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
+		RightLeg.setTextureOffset(32, 10).addBox(0.0F, 8.0F, -6.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
+		RightLeg.setTextureOffset(0, 25).addBox(-4.0F, 9.0F, -7.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
+		RightLeg.setTextureOffset(20, 34).addBox(-2.0F, 9.0F, 2.0F, 1.0F, 1.0F, 1.0F, 0.0F, true);
 
 		Tail = new ModelRenderer(this);
-		Tail.setRotationPoint(0.0F, 0.0F, 0.0F);
+		Tail.setRotationPoint(0.0F, 2.0F, 6.0F);
 		Body.addChild(Tail);
 		
 
 		Tail_r1 = new ModelRenderer(this);
-		Tail_r1.setRotationPoint(0.0F, 0.0F, 0.0F);
+		Tail_r1.setRotationPoint(0.0F, -2.0F, -6.0F);
 		Tail.addChild(Tail_r1);
 		setRotationAngle(Tail_r1, -0.5236F, 0.0F, 0.0F);
 		Tail_r1.setTextureOffset(0, 43).addBox(-2.0F, 0.0F, 18.0F, 3.0F, 2.0F, 2.0F, 0.0F, false);
@@ -217,28 +218,13 @@ public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 
 	@Override
 	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-		//TODO: Some sort of animation system
-	}
+		//TODO: Animation System
+		//v2: Now with proper group pivots!
 
-	public TRexModel(ModelRenderer body, ModelRenderer bodySpike_r1, ModelRenderer head, ModelRenderer bottomJaw, ModelRenderer leftUpperArm, ModelRenderer leftUpperArm_r1, ModelRenderer leftHand, ModelRenderer handNail_r1, ModelRenderer rightUpperArm, ModelRenderer rightUpperArm_r1, ModelRenderer rightHand, ModelRenderer handNail_r2, ModelRenderer leftLeg, ModelRenderer rightLeg, ModelRenderer tail, ModelRenderer tail_r1, ModelRenderer bb_main, ModelRenderer waist_r1) {
-		Body = body;
-		BodySpike_r1 = bodySpike_r1;
-		Head = head;
-		BottomJaw = bottomJaw;
-		LeftUpperArm = leftUpperArm;
-		LeftUpperArm_r1 = leftUpperArm_r1;
-		LeftHand = leftHand;
-		HandNail_r1 = handNail_r1;
-		RightUpperArm = rightUpperArm;
-		RightUpperArm_r1 = rightUpperArm_r1;
-		RightHand = rightHand;
-		HandNail_r2 = handNail_r2;
-		LeftLeg = leftLeg;
-		RightLeg = rightLeg;
-		Tail = tail;
-		Tail_r1 = tail_r1;
-		this.bb_main = bb_main;
-		Waist_r1 = waist_r1;
+		//We do get the entityIn so we can use local state machine vars!!
+		//The following should get a smooth consistent motion
+		RightUpperArm.rotateAngleX = MathHelper.cos(0.05F * ageInTicks) * 0.5F + 0.5f;
+		LeftUpperArm.rotateAngleX = MathHelper.cos(0.05F * ageInTicks) * 0.5F + 0.5f;
 	}
 
 	@Override
@@ -247,16 +233,9 @@ public class TRexModel<T extends TRexEntity> extends EntityModel<T> {
 		bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
-
-
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
 		modelRenderer.rotateAngleX = x;
 		modelRenderer.rotateAngleY = y;
 		modelRenderer.rotateAngleZ = z;
-	}
-
-	@Override
-	public Consumer<ModelRenderer> andThen(Consumer<? super ModelRenderer> after) {
-		return null;
 	}
 }
